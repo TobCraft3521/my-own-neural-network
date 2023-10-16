47fa2279f7da855f620f5433e4fb40d12cf7c851
TobCraft3521 <tobiashackenberg@outlook.de>
2023-10-16 21:52:49 +0200

    FINAL buxfix after 2h xD run & loss working

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-import NeuralNetwork from "./neuralnetwork.js"
+import { NeuralNetwork } from "./neuralnetwork.js"
+const nn = new NeuralNetwork(3, [2, 3])
+console.log(nn.loss([1, 3, 3], [4, 7, 6]))
+
--- neuralnetwork.js
+++ neuralnetwork.js
@@ -1,5 +1,44 @@
 export class NeuralNetwork {
-    weight1 = 1
+    layers
+    inputNodes
+    /**
+     * @param {number} inputNeurons 
+     * @param {number} outputNeurons 
+     * @param {Array} shape 
+     * 
+     * shape: [numberOfNeuronsLayer1,numberOfNeuronsLayer2,numberOfNeuronsLayer3...]
+     */
+    constructor(inputNeurons, shape) {
+        this.inputNodes = inputNeurons
+        let previousLayerOutputs = inputNeurons
+        this.layers = shape.map(neurons => {
+            const layer = new Layer(previousLayerOutputs, neurons)
+            previousLayerOutputs = neurons
+            return layer
+        })
+    }
+    run(inputs) {
+        let outputs = new Array(this.layers.length).fill([])
+        let index = 0
+        this.layers.forEach(layer => {
+            if (index == 0) {
+                outputs[index] = layer.calcOutputs(inputs)
+            } else {
+                outputs[index] = layer.calcOutputs(outputs[index - 1])
+            }
+            index++
+        })
+        return outputs[this.layers.length - 1]
+    }
+    loss(inputs, expectedOutputs) {
+        let outputs = this.run(inputs)
+        const outputLayer = this.layers[this.layers.length - 1]
+        let loss = 0
+        for (let i = 0; i < outputs.length; i++) {
+            loss += outputLayer.neuronCost(outputs[i], expectedOutputs[i])
+        }
+        return loss
+    }
 }
 
 export class Layer {
@@ -11,20 +50,25 @@ export class Layer {
         this.inputNodes = inputNeurons
         this.outputNodes = outputNeurons
         this.biases = new Array(outputNeurons).fill(0)
-        this.weights = new Array(inputNeurons).fill([])
+        this.weights = new Array(outputNeurons).fill([])
     }
     calcOutputs(inputs) {
-        let weightedInputs = []
+        let activations = []
         //loop through output neurons
         for (let i = 0; i < this.outputNodes; i++) {
             let weightedInput = this.biases[i]
             //loop through input neurons
-            for (let x = 0; x < this.outputNodes; x++) {
+            for (let x = 0; x < this.inputNodes; x++) {
+                console.log(this.weights)
                 weightedInput += inputs[x] * this.weights[x, i]
             }
-            weightedInputs[i] = weightedInput
+            activations[i] = sigmoid(weightedInput)
         }
-        return weightedInputs
+        return activations
+    }
+    neuronCost(activation, expected) {
+        let error = activation - expected
+        return error * error
     }
 }
 
